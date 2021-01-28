0c94e97739fab42d6da1092340cf97196e902905
francisco-1998 <francmarinc@gmail.com>
2021-01-28 08:50:11 -0500

    Terminando la modularizacion

---
+++ src/app/gifs/components/data-gifs/data-gifs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-gifs',
+  templateUrl: './data-gifs.component.html',
+  styleUrls: ['./data-gifs.component.css']
+})
+export class DataGifsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifPageComponent } from './gif-page/gif-page.component';
+import { SearchComponent } from './components/search/search.component';
+import { DataGifsComponent } from './components/data-gifs/data-gifs.component';
+
+
+
+@NgModule({
+  declarations: [GifPageComponent, SearchComponent,DataGifsComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    GifPageComponent
+  ]
+})
+export class GifsModule { }
